1f268b8062569e2ed21e6fc921bb9a1d28890991
Purvesh Meshri <73010284+Purvesh786@users.noreply.github.com>
2021-08-30 15:31:14 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom";
+import HookCounterOne from "../src/UseEffect/HookCounterOne";
 
 import App from "./App";
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-  rootElement
-);
+ReactDOM.render(<HookCounterOne />, rootElement);
